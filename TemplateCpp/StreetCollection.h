3da1d2c587f85cbb10af042e4dd6d11e5663171c
tox480 <dinhquoclam02@gmail.com>
2021-02-26 02:16:04 +0700

    input

--- TemplateCpp/StreetCollection.h
+++ TemplateCpp/StreetCollection.h
@@ -7,5 +7,15 @@ class StreetCollection
 {
 	static std::unordered_map<std::string, int> streetID;
 	static std::vector<std::string> streetName;
+	static int cnt = 0;
+	
+	StreetCollection() {
+		for (int i = 0; i < streetName.size(); i++) {
+			if (!streetID[streetName[i]]) {
+				streetID[streetName[i]] = ++cnt;
+			}
+		}
+	}
+
 };
 

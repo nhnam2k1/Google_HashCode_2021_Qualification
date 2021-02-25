621ecc633d3a568a27933cdd3c1f5f528c3692bc
FinisTP <53522152+FinisTP@users.noreply.github.com>
2021-02-26 01:28:03 +0700

    Input

--- TemplateCpp/Dataset.h
+++ TemplateCpp/Dataset.h
@@ -1,6 +1,18 @@
 #include <vector>
 #include <string>
 
+
+struct Street {
+	int begin, end;
+	std::string name;
+	int time;
+};
+
+struct Vehicle {
+	int path;
+	std::vector<int> paths;
+};
+
 #pragma once
 class Dataset
 {
@@ -8,6 +20,13 @@ public:
 	Dataset();
 	Dataset(std::string filename);
 	std::string GetFileName();
+	int duration; // D
+	int intersection; // I;
+	int street; // S
+	int vehicle; // V
+	int finishPoint; // F
+	std::vector<Street> streets;
+	std::vector<Vehicle> vehicles;
 private:
 	std::string filename;
 };
